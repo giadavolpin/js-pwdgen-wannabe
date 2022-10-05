919d0dfa8af66b4ed2040e952889e18bb3ecea43
giadavolpin <volpingiada@gmail.com>
2022-10-05 16:24:00 +0200

    help

---
+++ script.js
@@ -0,0 +1,17 @@
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+poi chiedi la sua età
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+(dove il 21 è in questo caso l'età) */
+
+const nomeUtente = prompt ('come ti chiami?'); 
+const cognomeUtente = prompt ('qual è il tuo cognome?')
+const coloreUtente = prompt ('qual è il tuo colore preferito?')
+const etaUtente = prompt ('quanti anni hai?')
+const saluto = '
+${nomeUtente}${cognomeUtente}${coloreUtente}${etaUtente}
+' ;
+
+const elemento = document.getElementById('utente');
+elemento.innerHTML = saluto; 
